04f492ad6b77a06dd10c3b5beca2fc6950bc67ca
Riza Ardiyanto <rizaardiyanto@Rizas-MacBook-Air.local>
2024-02-03 20:41:57 +0700

    feat: add login functionality

--- src/app/login/components/LoginForm.jsx
+++ src/app/login/components/LoginForm.jsx
@@ -2,37 +2,55 @@
 
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import Cookies from "js-cookie";
 
 export const LoginForm = () => {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  function handleSubmitLogin() {
-    console.log("Email: ", email);
-    console.log("Password: ", password);
+  async function handleSubmitLogin(e) {
+    e.preventDefault();
+
+    const res = await fetch("https://eventmakers-api.fly.dev/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    const { payload, token } = await res.json();
+
+    localStorage.setItem("user", JSON.stringify(payload));
+
+    Cookies.set("token", token);
     router.push("/dashboard");
   }
 
   return (
     <div>
-      <input
-        type="email"
-        placeholder="Email"
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      />
-      <input
-        type="password"
-        name="password"
-        id="password"
-        placeholder="password"
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-      />
-      <button onClick={handleSubmitLogin}>Submit</button>
+      <form onSubmit={handleSubmitLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button>Login</button>
+      </form>
     </div>
   );
 };
