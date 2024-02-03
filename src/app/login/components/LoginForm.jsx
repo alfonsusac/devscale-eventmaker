df9e92f51d60ba299e631dabb46f7646a0b866e5
Riza Ardiyanto <rizaardiyanto@Rizas-MacBook-Air.local>
2024-02-03 22:13:38 +0700

    feat: add basic styling to login form

--- src/app/login/components/LoginForm.jsx
+++ src/app/login/components/LoginForm.jsx
@@ -31,26 +31,31 @@ export const LoginForm = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmitLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <button>Login</button>
-      </form>
+    <div className="flex w-full h-full">
+      <div className="bg-blue-500 h-[100vh] w-[50vw]"></div>
+      <div className="flex items-center justify-center w-[50vw]">
+        <form onSubmit={handleSubmitLogin} className="flex flex-col gap-4 w-80">
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            className="input input-bordered input-primary w-full max-w-xs"
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            className="input input-bordered input-primary w-full max-w-xs"
+          />
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
