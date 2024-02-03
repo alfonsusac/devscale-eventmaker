e231397b0b7da0bda4ea5e23ff3445549849d9f7
mafatikhul ilmi <mafailmi@gmail.com>
2024-02-03 23:21:17 +0700

    feat: add function handle register

--- src/app/register/components/RegisterForm.jsx
+++ src/app/register/components/RegisterForm.jsx
@@ -1,24 +1,47 @@
+"use client";
 import React from "react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export const RegisterForm = () => {
+  const router = useRouter();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    console.log(name, email, password);
+  }
+
   return (
     <div className="flex w-full h-full">
       <div className="bg-blue-500 h-[100vh] w-[50vw]"></div>
       <div className="flex items-center justify-center w-[50vw]">
-        <form className="flex flex-col gap-4 w-80">
+        <form onSubmit={handleRegister} className="flex flex-col gap-4 w-80">
           <input
             type="text"
             placeholder="Name"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
             className="input input-bordered input-primary w-full max-w-xs"
           />
           <input
             type="email"
             placeholder="Email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             className="input input-bordered input-primary w-full max-w-xs"
           />
           <input
             type="password"
             name="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             id="password"
             placeholder="password"
             className="input input-bordered input-primary w-full max-w-xs"
