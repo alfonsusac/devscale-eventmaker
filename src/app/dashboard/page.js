5f0b79017ec14a606cfc99e70af36d558bb6bd00
mafatikhul ilmi <mafailmi@gmail.com>
2024-02-04 16:57:07 +0700

    style: dashboard skeleton

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,5 +1,6 @@
 import React from "react";
+import { DashboardPage } from "@/components/dashboard/components/DashboardPage";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return <DashboardPage />;
 }
