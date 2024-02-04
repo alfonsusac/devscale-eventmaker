582ed9945baf51ee2f545c3db33308baaa7d6cd1
mafatikhul ilmi <mafailmi@gmail.com>
2024-02-04 12:49:31 +0700

    feat: grouping the component element inside components directory

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <main className="max-w-6xl m-auto">
+      <section className="flex justify-center">
+        <h2>EventMaker</h2>
+        <h4>Best event listing in Indonesia</h4>
+        <button>Create New Event</button>
+      </section>
+      <section className="grid grid-cols-4 gap-4">
+        <div className="border-2 rounded-md">card1</div>
+        <div className="border-2 rounded-md">card2</div>
+        <div className="border-2 rounded-md">card3</div>
+        <div className="border-2 rounded-md">card4</div>
+      </section>
+    </main>
+  );
 }
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { RegisterForm } from "./components/RegisterForm";
+import { RegisterForm } from "../../components/register/components/RegisterForm";
 
 export default function Register() {
   return (
