8f27f97f9a1a224655f7d4efd9b3569997a8b641
Riza Maulana Ardiyanto <83811723+rizaardiyanto1412@users.noreply.github.com>
2024-02-04 16:38:41 +0700

    feat: add react hot toast (#38)
    Co-authored-by: Riza Ardiyanto <rizaardiyanto@Rizas-MacBook-Air.local>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import { Header } from "@/components/Header";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Toaster />
         <Header />
         {children}
       </body>
