c7aab0581e5dae0e3fd4fa6b52e30467286b1dae
Riza Maulana Ardiyanto <83811723+rizaardiyanto1412@users.noreply.github.com>
2024-02-04 19:18:50 +0700

    feat: separate login hooks (#33)
    * feat: separate login hooks
    
    * feat: put login func in fetchAPI.js
    
    * fix: typo in package.json
    
    * fix: Login UI following the design
    
    * fix: separating fetching and hook
    
    * fix: fix error handling and alt tag logo
    
    ---------
    
    Co-authored-by: Riza Ardiyanto <rizaardiyanto@Rizas-MacBook-Air.local>
    Co-authored-by: Alfonsus Ardani <alfonsusac@gmail.com>

--- src/app/login/components/LoginForm.jsx
+++ src/app/login/components/LoginForm.jsx
@@ -1,38 +1,47 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import { requestLogin } from "@/lib/fetchAPI";
 import Cookies from "js-cookie";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 export const LoginForm = () => {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  async function handleSubmitLogin(e) {
+  const handleSubmitLogin = async (e) => {
+    setLoading(true);
     e.preventDefault();
+    try {
+      const { payload, token } = await requestLogin(email, password);
 
-    const res = await fetch("https://eventmakers-api.fly.dev/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    });
-    const { payload, token } = await res.json();
+      if (!payload || !token) {
+        setLoading(false);
+        toast.error("User not found, please try again.");
+        return;
+      }
 
-    localStorage.setItem("user", JSON.stringify(payload));
-
-    Cookies.set("token", token);
-    router.push("/dashboard");
-  }
+      localStorage.setItem("user", JSON.stringify(payload));
+      Cookies.set("token", token);
+      toast.success("Logged in successfully");
+      setTimeout(() => {
+        router.push("/dashboard");
+        setLoading(false);
+      }, 2000);
+    } catch (error) {
+      setLoading(false);
+      toast.error(error.message);
+    }
+  };
 
   return (
-    <div className="flex w-full h-full">
-      <div className="bg-blue-500 h-[100vh] w-[50vw]"></div>
+    <div className="flex flex-col items-center justify-center m-auto h-[100vh] gap-10">
+      <Image src="/logo.png" width={250} height={200} alt="logo" />
+      <h1 className="text-4xl">LOGIN</h1>
       <div className="flex items-center justify-center w-[50vw]">
         <form onSubmit={handleSubmitLogin} className="flex flex-col gap-4 w-80">
           <input
@@ -53,7 +62,9 @@ export const LoginForm = () => {
             }}
             className="input input-bordered input-primary w-full max-w-xs"
           />
-          <button className="btn btn-primary">Login</button>
+          <button className="btn btn-primary ">
+            {loading ? "Logging in..." : "Login"}
+          </button>
         </form>
       </div>
     </div>
