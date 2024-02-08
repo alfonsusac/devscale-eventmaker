1e0c69b424f3ca314641fb1d95df9c792c706b2e
mafatikhul ilmi <mafailmi@gmail.com>
2024-02-08 16:09:20 +0700

    style: update style register UI

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-import { RegisterForm } from "../../components/register/components/RegisterForm";
-=======
 import { RegisterForm } from "./components/RegisterForm";
->>>>>>> eventmakers-stage1
 
 export default function Register() {
   return (
