d6d97edb7f3512ae416fc644e5c7456f8ac3e8aa
Riza Maulana Ardiyanto <83811723+rizaardiyanto1412@users.noreply.github.com>
2024-02-11 15:10:42 +0700

    feat: add authentication function (#55)
    * feat: add jose
    
    * feat: add jwt-decode
    
    * feat: add function session login and logout
    
    * feat: add server session
    
    * fix: using login function
    
    * feat: add logout function to header
    
    * fix: fix router issue
    
    ---------
    
    Co-authored-by: Riza Ardiyanto <rizaardiyanto@Rizas-MacBook-Air.local>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
+"use client";
+
+import { logout } from "@/lib/session";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
 export const Header = () => {
+  const router = useRouter();
+
+  function handleLogout() {
+    logout();
+    toast.success("Logout success!");
+    router.push("/");
+  }
+
   return (
     <header className="flex justify-between p-3">
       <h3 className="text-base font-bold">eventMakers.</h3>
       <div className="flex space-x-2 items-center">
         <p className="text-sm">Hi, Mafatikhul Ilmi !</p>
-        <button className="text-sm border-2 border-indigo-600 rounded-xl py-1 px-2">
+        <button
+          onClick={handleLogout}
+          className="text-sm border-2 border-indigo-600 rounded-xl py-1 px-2"
+        >
           Logout
         </button>
       </div>
---
+++ src/lib/server-session.js
@@ -0,0 +1,8 @@
+import { cookies } from "next/headers";
+import { jwtDecode } from "jwt-decode";
+
+export function session() {
+  const token = cookies().get("token")?.value;
+  const userData = jwtDecode(token);
+  return userData;
+}
