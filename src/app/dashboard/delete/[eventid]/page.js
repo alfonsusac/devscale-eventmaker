5b7b756f09b96110f035f9fe565be0e8c277175a
mafatikhul ilmi <mafailmi@gmail.com>
2024-02-11 10:58:37 +0700

    style: add delete event page

---
+++ src/app/dashboard/delete/[eventid]/page.js
@@ -0,0 +1,10 @@
+import { DeleteEvent } from "@/components/DeleteEvent";
+import React from "react";
+
+export default function Delete() {
+  return (
+    <div>
+      <DeleteEvent />
+    </div>
+  );
+}
