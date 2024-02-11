b4cbbfc01201725387750ca212a98b734612b7d2
Mafatikhul Ilmi <mafailmi@gmail.com>
2024-02-11 16:51:15 +0700

    46 delete event basic UI (#59)
    * style: add delete event page
    
    * style: delete page confirmation
    
    * chore: added comment
    
    ---------
    
    Co-authored-by: Alfonsus Ardani <alfonsusac@gmail.com>

--- src/components/DeleteEvent.jsx
+++ src/components/DeleteEvent.jsx
@@ -3,7 +3,7 @@ import { React, useState } from "react";
 import Link from "next/link";
 
 export const DeleteEvent = () => {
-  const [prop] = useState({
+  const [prop] = useState({ // Todo: no longer need this
     title: "",
     desc: "",
     dateTime: "",
@@ -12,54 +12,28 @@ export const DeleteEvent = () => {
   return (
     <div className="flex flex-col items-center justify-center m-auto  gap-5">
       <h1 className="text-2xl font-semibold">Delete Event</h1>
+      <p>Are you sure you want to delete this event? </p>
       <div className="flex flex-col items-center justify-center space-y-2 w-[50vw]">
-        <div className="flex flex-col gap-4 w-80">
-          <input
-            type="text"
-            name="title"
-            placeholder="Title"
-            value={prop.title}
-            className="input input-sm input-bordered input-primary w-full max-w-xs"
-          />
-          <textarea
-            className="textarea textarea-bordered textarea-primary"
-            name="description"
-            value={prop.desc}
-            placeholder="Description"
-          ></textarea>
+        <div className="gap-4 space-y-4 w-80">
+          <div className="border border-gray-300 rounded-lg p-3">
+            <h2 className="font-bold">Event Title</h2>
+            <p>Event Date : 11 Feb 2024</p>
+          </div>
 
-          <input
-            type="date"
-            name="eventdate"
-            placeholder="Event Date"
-            value={prop.dateTime}
-            className="input input-sm input-bordered input-primary w-full max-w-xs"
-          />
-
-          <button
-            className="btn btn-sm btn-primary"
-            onClick={() => document.getElementById("my_modal_1").showModal()}
-          >
-            Delete
-          </button>
+          <div className="grid grid-cols-2 gap-3">
+            <button
+              className="btn btn-sm btn-error"
+              onClick={handleDeleteEvent}
+            >
+              Delete
+            </button>
+            <button className="btn btn-sm btn-default">Cancel</button>
+          </div>
         </div>
         <Link href={"/dashboard"}>
           <button className="btn btn-sm btn-outline btn-secondary">Back</button>
         </Link>
       </div>
-      <dialog id="my_modal_1" className="modal">
-        <div className="modal-box">
-          <p className="py-4">Are you sure to delete the data ?</p>
-          <div className="modal-action">
-            <form method="dialog" className="space-x-2">
-              <button className="btn btn-error" onClick={handleDeleteEvent}>
-                Delete
-              </button>
-              <button className="btn">Close</button>
-            </form>
-          </div>
-        </div>
-      </dialog>
     </div>
   );
 };
