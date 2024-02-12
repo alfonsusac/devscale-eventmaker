3acd434606d2c9c4fd9016923ddb1b02c4e75c97
Riza Ardiyanto <rizaardiyanto@Rizas-MacBook-Air.local>
2024-02-12 14:23:29 +0700

    fix: matcher configuration

--- src/middleware.js
+++ src/middleware.js
@@ -3,23 +3,37 @@ import { session } from "./lib/server-session";
 import * as jose from "jose";
 
 export async function middleware(request) {
-  const { token } = session();
-  const secretKey = process.env.SECRET_KEY;
-  const secret = new TextEncoder().encode(secretKey);
+  //parsing the URL
+  const url = new URL(request.url);
+  const path = url.pathname;
+  //verify token on dashboard URL
+  if (path === "/dashboard") {
+    const { token } = session();
+    const secretKey = process.env.SECRET_KEY;
+    const secret = new TextEncoder().encode(secretKey);
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
 
-  //validating token
-  try {
-    await jose.jwtVerify(token, secret);
-    return NextResponse.next();
-  } catch (error) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    //validating token
+    try {
+      await jose.jwtVerify(token, secret);
+      return NextResponse.next();
+    } catch (error) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
   }
 }
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!_next/static|_next/image|favicon.ico).*)",
+  ],
 };
