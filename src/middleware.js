edc35d894a44fd9563a11a304ee668ba1969745d
Alfonsus Ardani <alfonsusac@gmail.com>
2024-02-13 22:48:32 +0700

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -11,13 +11,13 @@ export async function middleware(request) {
     const { token } = session();
     const secretKey = process.env.SECRET_KEY;
     const secret = new TextEncoder().encode(secretKey);
-
+    
     if (!token) {
       return NextResponse.redirect(new URL("/login", request.url));
     }
-
-    //validating token
+    
     try {
+      //validating token
       await jose.jwtVerify(token, secret);
       return NextResponse.next();
     } catch (error) {
