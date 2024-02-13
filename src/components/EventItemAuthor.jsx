1a840ec011e259268f7bfa1e8229f47e7455c46b
Alfonsus Ardani <alfonsusac@gmail.com>
2024-02-14 00:43:22 +0900

    Update EventItemAuthor.jsx (#80)

--- src/components/EventItemAuthor.jsx
+++ src/components/EventItemAuthor.jsx
@@ -2,9 +2,9 @@ import { requestUser } from "@/lib/fetchAPI";
 import { session } from "@/lib/server-session";
 
 export const EventItemAuthor = async ({ userid }) => {
-  const { token } = session();
   let data;
   try {
+    const { token } = session();
     data = await requestUser(token, userid);
   } catch (error) {
     console.error("An error occurred while fetching the user:", userid, error);
