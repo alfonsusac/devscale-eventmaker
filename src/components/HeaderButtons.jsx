f6a18225df093cf94f178c73faae084fca339aa2
Tanesya <tanesya.t@gmail.com>
2024-02-13 11:33:37 +0700

    fix: move handleLogout inside HeaderButtons component

--- src/components/HeaderButtons.jsx
+++ src/components/HeaderButtons.jsx
@@ -4,12 +4,6 @@ import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 import { login, logout } from "@/lib/session";
 
-function handleLogout(router) {
-  logout();
-  toast.success("Logout success!");
-  router.push("/");
-}
-
 const MainButton = ({ funcOnClick, borderCol, text }) => {
   const classNames = `text-sm border-2 ${borderCol} rounded-xl py-1 px-2`;
   return (
@@ -22,16 +16,22 @@ const MainButton = ({ funcOnClick, borderCol, text }) => {
 export const HeaderButtons = ({ type }) => {
   const router = useRouter();
 
+  function handleLogout() {
+    logout();
+    toast.success("Logout success!");
+    router.push("/");
+  }
+
   if (type === "Guest") {
     return (
       <div className="flex gap-2">
         <MainButton
-          funcOnClick={() => router.push('/login')}
+          funcOnClick={() => router.push("/login")}
           borderCol="border-indigo-600"
           text="Login"
         />
         <MainButton
-          funcOnClick={() => router.push('/register')}
+          funcOnClick={() => router.push("/register")}
           borderCol="border-green-600"
           text="Register"
         />
@@ -41,12 +41,12 @@ export const HeaderButtons = ({ type }) => {
     return (
       <div className="flex gap-2">
         <MainButton
-          funcOnClick={() => router.push('/dashboard')}
+          funcOnClick={() => router.push("/dashboard")}
           borderCol="border-indigo-600"
           text="Go to Dashboard"
         />
         <MainButton
-          funcOnClick={handleLogout(router)}
+          funcOnClick={handleLogout}
           borderCol="border-rose-600"
           text="Logout"
         />
