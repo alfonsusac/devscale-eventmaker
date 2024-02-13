dd4ac87254b4e448e22a706ee73d579a23d2c3fb
Tanesya <tanesya.t@gmail.com>
2024-02-13 11:16:34 +0700

    feat: user greetings based on user name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,15 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { HeaderButtons } from "@/components/HeaderButtons";
 
 export const Header = ({ session }) => {
-  const router = useRouter();
-
   let UserGreetings;
   let Buttons;
 
   if (!session) {
     Buttons = <HeaderButtons type="Guest" />;
   } else {
-    UserGreetings = <p className="text-sm">Hi, Mafatikhul Ilmi !</p>;
+    UserGreetings = `Hi, ${session.userData.name}!`
     Buttons = <HeaderButtons type="User" />;
   }
 
@@ -20,8 +17,8 @@ export const Header = ({ session }) => {
     <header className="flex justify-between p-3">
       <h3 className="text-base font-bold">eventMakers.</h3>
       <div className="flex space-x-2 items-center">
-        {UserGreetings}
-        {Buttons}
+      <p className="text-sm">{UserGreetings}</p>
+      {Buttons}
       </div>
     </header>
   );
