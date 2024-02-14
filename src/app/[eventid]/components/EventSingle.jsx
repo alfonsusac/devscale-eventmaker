9b4d522f0246fd4c5f1de292d22639cf1e367023
Riza Ardiyanto <rizaardiyanto@Rizas-MacBook-Air.local>
2024-02-14 14:49:42 +0700

    feat: add bear avatar

--- src/app/[eventid]/components/EventSingle.jsx
+++ src/app/[eventid]/components/EventSingle.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Image from "next/image";
-import Avatar from "boring-avatars";
 import { EventItemAuthor } from "@/components/EventItemAuthor";
 import Link from "next/link";
 import { session } from "@/lib/server-session";
+import { createAvatar } from "@dicebear/core";
+import { lorelei } from "@dicebear/collection";
 
 export default function EventSingle({ event }) {
   const sessionData = session();
   const token = sessionData && sessionData.token ? sessionData.token : null;
+  const avatar = createAvatar(lorelei, {
+    seed: "Jasper",
+    backgroundColor: ["ffdfbf"],
+    size: 32,
+  });
+  const svg = avatar.toString();
 
   return (
     <div className="w-[500px] flex flex-col gap-4 m-4">
@@ -28,9 +35,15 @@ export default function EventSingle({ event }) {
           <h1>{event.events.title}</h1>
           <p>{event.events.description}</p>
           {token && (
-            <p>
-              Author: <EventItemAuthor userid={event.events.author} />
-            </p>
+            <div className="flex flex-col gap-3">
+              <p>Penyelenggara:</p>
+              <div className="flex gap-2 items-center">
+                <div dangerouslySetInnerHTML={{ __html: svg }} />
+                <p>
+                  <EventItemAuthor userid={event.events.author} />
+                </p>
+              </div>
+            </div>
           )}
         </div>
       </div>
