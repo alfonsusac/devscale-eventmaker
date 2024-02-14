e8f66a8350a27c414f9d48c2fbc8e4d39d1c581c
Riza Maulana Ardiyanto <83811723+rizaardiyanto1412@users.noreply.github.com>
2024-02-14 10:27:49 +0700

    fix: add link to logo (#91)
    Co-authored-by: Riza Ardiyanto <rizaardiyanto@Rizas-MacBook-Air.local>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { HeaderButtons } from "@/components/HeaderButtons";
+import Link from "next/link";
 
 export const Header = ({ session }) => {
   let UserGreetings;
@@ -9,16 +10,18 @@ export const Header = ({ session }) => {
   if (!session) {
     Buttons = <HeaderButtons type="Guest" />;
   } else {
-    UserGreetings = `Hi, ${session.userData.name}!`
+    UserGreetings = `Hi, ${session.userData.name}!`;
     Buttons = <HeaderButtons type="User" />;
   }
 
   return (
     <header className="flex justify-between p-3">
-      <h3 className="text-base font-bold">eventMakers.</h3>
+      <Link href="/">
+        <h3 className="text-base font-bold">eventMakers.</h3>
+      </Link>
       <div className="flex space-x-2 items-center">
-      <p className="text-sm">{UserGreetings}</p>
-      {Buttons}
+        <p className="text-sm">{UserGreetings}</p>
+        {Buttons}
       </div>
     </header>
   );
