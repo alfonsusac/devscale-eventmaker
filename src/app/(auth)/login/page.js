e0ee80b5cbd4589bdbb3f152cf79225f22d3f3ec
Alfonsus Ardani <alfonsusac@gmail.com>
2024-02-15 15:31:13 +0700

    feat: added image to login and register page

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,9 +1,18 @@
 import { LoginForm } from "./components/LoginForm";
+import Image from 'next/image'
 
 export default function Login() {
   return (
-    <div>
+    <>
       <LoginForm />
-    </div>
+      <div className="relative overflow-hidden bg-black hidden md:block">
+        <Image
+          src="https://images.unsplash.com/photo-1492684223066-81342ee5ff30?q=1&w=1920"
+          alt="Conference Image"
+          fill sizes="50vw"
+          objectFit="cover"
+        />
+      </div>
+    </>
   );
 }
--- src/app/(auth)/register/page.js
+++ src/app/(auth)/register/page.js
@@ -1,10 +1,18 @@
-import React from "react";
 import { RegisterForm } from "./components/RegisterForm";
+import Image from 'next/image'
 
 export default function Register() {
   return (
-    <div>
+    <>
       <RegisterForm />
-    </div>
+      <div className="relative overflow-hidden bg-black hidden md:block">
+        <Image
+          src="https://images.unsplash.com/photo-1587825140708-dfaf72ae4b04?q=1&w=1920"
+          alt="Conference Image"
+          fill sizes="50vw"
+          objectFit="cover"
+        />
+      </div>
+    </>
   );
 }
